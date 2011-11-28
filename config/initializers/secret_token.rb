5f9c52143dfc020d6a0b31639bc364301c70dce4
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-11-28 17:21:07 +0000

    Vanilla Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsyncRails::Application.config.secret_token = '161629b4440b782b68194aa306647d8a50731e6f7f270517c5aab1fb5c6982acde1b831d0137e6e72b6ea102e66aa3b40674d61b477089895855b0d94a9d4f34'
